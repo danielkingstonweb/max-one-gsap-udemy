24c93417016fdfd69aa32f99766778669ab68686
Daniel Kingston <dnlkingston75@gmail.com>
2024-01-11 11:50:42 +1300

    gsap.to lesson one complete

--- Resources/1.Basics of Greensock/StartingExamples/script.js
+++ Resources/1.Basics of Greensock/StartingExamples/script.js
@@ -1,31 +1,32 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+  // GSAP Object
+  const greenSquare = document.querySelector('.greenSquare')
 
-// GSAP Object
-const greenSquare = document.querySelector('.greenSquare');
+  gsap.to(greenSquare, {
+    duration: 2,
+    x: 300,
+    rotation: 360,
+    opacity: 0.5,
+    backgroundColor: 'red',
+    ease: 'power1.out',
+  })
 
+  // gsap.from()
+  const orangeSquare = document.querySelector('.orangeSquare')
 
-// gsap.from()
-const orangeSquare = document.querySelector('.orangeSquare');
+  // gsap.to()
+  const purpleSquare = document.querySelector('.purpleSquare')
 
+  // gsap.fromTo()
+  const blueSquare = document.querySelector('.blueSquare')
 
-// gsap.to()
-const purpleSquare = document.querySelector('.purpleSquare');
+  // Delay & Repeat
+  const circleYellow = document.querySelector('.circleYellow')
+  const circleRed = document.querySelector('.circleRed')
+  const circleGreen = document.querySelector('.circleGreen')
 
+  // Ease
+  const circleBlue = document.querySelector('.circleBlue')
 
-// gsap.fromTo()
-const blueSquare = document.querySelector('.blueSquare');
-
-
-// Delay & Repeat
-const circleYellow = document.querySelector('.circleYellow');
-const circleRed = document.querySelector('.circleRed');
-const circleGreen  = document.querySelector('.circleGreen');
-
-
-// Ease
-const circleBlue = document.querySelector('.circleBlue');
-
-// Stagger
-
-
-});
+  // Stagger
+})
