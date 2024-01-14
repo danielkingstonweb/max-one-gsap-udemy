33c4456d06668768be4cb7053e9c871c98a20198
Daniel Kingston <dnlkingston75@gmail.com>
2024-01-14 22:51:26 +1300

    ScrollTrigger basic implimentation complete

--- Resources/3.Animate on Scroll/StartingExamples/script.js
+++ Resources/3.Animate on Scroll/StartingExamples/script.js
@@ -15,6 +15,20 @@ document.addEventListener('DOMContentLoaded', function () {
   // #f3a712
   const redSquare = document.querySelector('.redSquare')
 
+  gsap.to(redSquare, {
+    duration: 10,
+    x: 700,
+    rotation: 360,
+    backgroundColor: '#f3a712',
+    scrollTrigger: {
+      trigger: redSquare,
+      markers: true,
+      start: 'top 75%',
+      end: 'bottom 25%',
+      toggleActions: 'restart pause reverse reset',
+    },
+  })
+
   // ScrollTrigger Scrub & Pin
   const tanSection = document.querySelector('.tanSection')
   const redBar = document.querySelector('.redBar')
